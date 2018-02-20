ecf9b7676753babcc3f9d9bbf03c571cdced2d54
Jorgelduarte <duarte.junior@gmail.com>
2018-02-20 03:26:54 +0000

    Defining Callbacks - print_uppercase.js, print_lowercase.js, print_reverse.js

---
+++ callback/print_uppercase.js
@@ -0,0 +1,14 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/uppercase.html'
+  };
+  
+  function printUpperCase (html) {
+    var uppercase = html.toUpperCase()
+    console.log(uppercase);
+  
+  }
+  
+  getHTML(requestOptions, printUpperCase);
